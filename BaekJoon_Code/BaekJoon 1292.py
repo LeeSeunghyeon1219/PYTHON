7fc3a5de8272ee2f1e785aa13b7357f411d0b065
LeeSeunghyeon1219 <44192806+LeeSeunghyeon1219@users.noreply.github.com>
2022-02-03 20:14:59 +0900

    ## BaekJoon 11653 소인수분해

---
+++ BaekJoon_Code/BaekJoon 1292.py
@@ -0,0 +1,18 @@
+## BaekJoon 1292 쉽게 푸는 문제
+
+import sys
+
+A, B = map(int, sys.stdin.readline().split())
+data_list = []
+num = 1
+cnt = 0
+result = 0
+for i in range(B):
+    cnt = cnt + 1
+    if i + 1 >= A:
+        result = result + num
+    if cnt == num:
+        num = num + 1
+        cnt = 0
+print(result)
+
