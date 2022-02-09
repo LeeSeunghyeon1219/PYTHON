7c207c0e99369b86b316e27bce29d348cd6e87af
LeeSeunghyeon1219 <44192806+LeeSeunghyeon1219@users.noreply.github.com>
2022-02-09 21:36:58 +0900

    ## BaekJoon 1929 - 소수구하기 (에라토스테네스의 체)

--- BaekJoon_Code/BaekJoon 1929.py
+++ BaekJoon_Code/BaekJoon 1929.py
@@ -1,12 +1,14 @@
+## BaekJoon 1929 - 소수구하기 (에라토스테네스의 체)
 import sys
 
-N,M=map(int,sys.stdin.readline().split())
+M,N=map(int,sys.stdin.readline().split())
 
-for i in range(N,M+1):
-    check=True
-    for j in range(2,i):
-        if i%j==0:
-            check=False
-            break
-    if check==True:
-        print(i,end="\n")
+check_list=[False,False] + [True]*(N-1)
+for i in range(2,int((N+1)**0.5)+1):
+    if check_list[i]==True:
+        for j in range(2*i,N+1,i):
+            check_list[j]=False
+
+
+for j in range(M,N+1):
+    if check_list[j]==True: print(j)
