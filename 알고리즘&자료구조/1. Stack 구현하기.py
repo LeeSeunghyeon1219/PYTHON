eb95197ac0bb5348d954a54d22900a30442f953c
LeeSeunghyeon1219 <44192806+LeeSeunghyeon1219@users.noreply.github.com>
2022-03-05 17:20:08 +0900

    ## Stack Code Update

--- 알고리즘&자료구조/1. Stack 구현하기.py
+++ 알고리즘&자료구조/1. Stack 구현하기.py
@@ -1,21 +1,36 @@
+MAX_STACK_SIZE = 3
+
 class Stack:
     def __init__(self):
         self.stack = []
+        self.top = -1
 
     def isEmpty(self):
-        if len(self.stack) == 0:
+        if self.top < 0:
+            return True
+        else:
+            return False
+
+    def isfull(self):
+        if self.top >= MAX_STACK_SIZE - 1:
             return True
         else:
             return False
 
     def push(self, data):
-        self.stack.append(data)
+        if self.isfull():
+            print("Stak is Full")
+        else:
+            self.stack.append(data)
+            self.top += 1
 
     def pop(self):
         if self.isEmpty():
             return "Stack is empty"
         else:
-            return self.stack.pop()
+            value = self.stack.pop()
+            self.top -= 1
+            return value
 
     def top(self):
         if self.isEmpty():
